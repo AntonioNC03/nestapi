8bd9eb71729d21d14d3b31dad8e3498985033769
Antonio <antonionavarrocollado03@gmail.com>
2025-02-02 22:18:40 +0100

    Practica nest

---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './usuario/usuario.module';
+import { AlumnoModule } from './alumno/alumno.module';
+import { BibliotecaModule } from './biblioteca/biblioteca.module';
+import { PokemonsModule } from './pokemons/pokemons.module';
+
+@Module({
+  imports: [ConfigModule.forRoot({isGlobal:true,}), 
+    TypeOrmModule.forRoot({
+      name:'base1',
+      type:'mysql',
+      host:process.env.URL,
+      port:3306,
+      username:process.env.USUARIO,
+      password:process.env.PASSWORD,
+      database: process.env.DBNAME,
+      autoLoadEntities:true,//Esta es la mejor opción para que coja sólo las que haya en módulo
+      synchronize:true
+    }),
+    TypeOrmModule.forRoot({
+      name:'base2',
+      type:'mysql',
+      host:process.env.URL,
+      port:3306,
+      username:process.env.USUARIO,
+      password:process.env.PASSWORD,
+      database: process.env.DBNAME2,
+      autoLoadEntities:true,//Esta es la mejor opción para que coja sólo las que haya en módulo
+      synchronize:true
+    }),
+    UsuarioModule,
+    AlumnoModule,
+    BibliotecaModule,
+    PokemonsModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
